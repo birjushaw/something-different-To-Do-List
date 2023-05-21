b186f5cf47e6da64093ddb9ec86a9c81343e1158
Birju Shaw <123962534+birjushaw@users.noreply.github.com>
2023-05-20 23:11:15 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const inputBox  = document.getElementById("input-box");
+const listcontainer  = document.getElementById("list-container");
+
+
+function AddTask(){
+   if(inputBox.value === ""){
+    alert("You must write something");
+   }else{
+    let li = document.createElement("li")
+    li.innerHTML = inputBox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+   }
+   inputBox.value = "";
+   saveData()
+}
+
+
+listcontainer.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.add("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+})
+
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML)
+}
+
+function showData(){
+    listcontainer.innerHTML = localStorage.getItem("data")
+}
+showData();
